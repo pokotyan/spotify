0fc7f119e1a81285c7d9613521e62d3dff38ae98
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2018-05-20 10:52:32 +0900

    homeのcontainerをpureなfunctionに変更

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,91 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Dimmer, Loader, Image } from 'semantic-ui-react'
+import { Dimmer, Loader } from 'semantic-ui-react';
 import * as spotifyActions from '../actions/spotify';
 import SideBar from '../components/SideBar';
 import Main from '../components/Main';
 
-class Home extends Component {
-  // componentWillUpdate(nextProps, nextState) {
-  //   console.log(nextProps);
-  //   console.log(nextState);
-  //   const {
-  //     spotifyActions: {
-  //       fetchDevice,
-  //     },
-  //     spotify: {
-  //       auth: {
-  //         accessToken,
-  //       },
-  //     },
-  //   } = nextProps;
+const Home = (props) => {
+  const {
+    spotifyActions: {
+      fetchDevice,
+      search,
+      play,
+    },
+    spotify,
+  } = props;
 
-  //   fetchDevice(accessToken);
-  // }
-
-  // async componentWillReceiveProps(nextProps) {
-  //   const {
-  //     spotifyActions: {
-  //       fetchDevice,
-  //     },
-  //     spotify: {
-  //       auth: {
-  //         accessToken,
-  //       },
-  //     },
-  //   } = nextProps;
-
-  //   await fetchDevice(accessToken);
-  // }
-
-  // 本当はこのコンポーネントのマウント時に裏側でdevice取得したいけど、マウント直後はまだaccessTokenがpropsに入っていない。
-  // propsにaccessToken入ってから更新系のライフサイクル使ってdevice取得したら無限ループになるし、どうしたらいいのか
-  render() {
-    const {
-      spotifyActions: {
-        fetchDevice,
-        search,
-        play,
-      },
-      // spotify: {
-      //   auth: {
-      //     accessToken,
-      //   },
-      // },
-    } = this.props;
-
-    return (
-      <div className="app">
-        <div className="sidebar">
-          {this.props.spotify.auth.accessToken ?
-            <SideBar
-              fetchDevice={fetchDevice}
-              search={search}
-              accessToken={this.props.spotify.auth.accessToken}
-              refreshToken={this.props.spotify.auth.refreshToken}
-            /> :
-            <Dimmer active>
-              <Loader>Loading</Loader>
-            </Dimmer>
-          }
-        </div>
-        <div className="main">
-          {Object.keys(this.props.spotify.search).length &&
-            <Main
-              search={this.props.spotify.search}
-              play={play}
-              accessToken={this.props.spotify.auth.accessToken}
-              refreshToken={this.props.spotify.auth.refreshToken}
-            />
-          }
-        </div>
-        <div className="footer" />
+  return (
+    <div className="app">
+      <div className="sidebar">
+        {spotify.auth.accessToken ?
+          <SideBar
+            fetchDevice={fetchDevice}
+            search={search}
+            accessToken={spotify.auth.accessToken}
+            refreshToken={spotify.auth.refreshToken}
+          /> :
+          <Dimmer active>
+            <Loader>Loading</Loader>
+          </Dimmer>
+        }
       </div>
-    );
-  }
-}
+      <div className="main">
+        {Object.keys(spotify.search).length &&
+          <Main
+            search={spotify.search}
+            play={play}
+            accessToken={spotify.auth.accessToken}
+            refreshToken={spotify.auth.refreshToken}
+          />
+        }
+      </div>
+      <div className="footer" />
+    </div>
+  );
+};
+
 Home.propTypes = {
   spotifyActions: PropTypes.object.isRequired,
   spotify: PropTypes.object,
