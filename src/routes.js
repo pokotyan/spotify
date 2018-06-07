f9128767a97089913922dd2d5dcdadcfb49aff04
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2018-06-07 10:58:06 +0900

    react-router-configの導入

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
-import About from './container/About';
-import Top from './container/Top';
-import Pokemon from './container/Pokemon';
-import CallBack from './container/SpotifyCallBack';
-import Home from './container/Home';
-import NotFound from './components/NotFound';
+import { renderRoutes } from 'react-router-config'
+import routes from './routing'
 
-// @todo react-router-reduxは使わないようにする。
-// ConnectedRouterはProviderからのstoreを自動で利用する
-// 参考：https://github.com/ReactTraining/react-router/tree/master/packages/react-router-redux
-const Routes = () => {
+const Routes = () => renderRoutes(routes)
+
+export default () => {
   return (
     <Switch>
-      <Route exact path="/" component={Top} />
-      <Route path="/callback" component={CallBack} />
-      <Route path="/home" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/pokemon" component={Pokemon} />
-      {/* <Route component={NotFound} /> */}
+      <Routes />
     </Switch>
   );
 };
-
-export default Routes;
---
+++ src/routing.js
@@ -0,0 +1,35 @@
+import Top from './container/Top';
+import CallBack from './container/SpotifyCallBack';
+import Home from './container/Home';
+import About from './container/About';
+import Pokemon from './container/Pokemon';
+import NotFound from './components/NotFound';
+ 
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Top,
+  },
+  {
+    path: '/callback',
+    component: CallBack,
+  },
+  {
+    path: '/home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    component: About,
+  },
+  {
+    path: '/pokemon',
+    component: Pokemon,
+  },
+  {
+    component: NotFound,
+  },
+]
+ 
+export default routes
