9a26bab49091ca1525ff28c51c47fd602c3dca4a
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2018-06-10 13:47:09 +0900

    cssのディレクトリ構成変更

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './App';
 import './style/reset.scss';
 import './style/top.scss';
-import './style/app.scss';
-import './style/main/main.scss';
+import './style/layout.scss';
 import './style/main/item-list.scss';
 import './style/sidebar/sidebar.scss';
 import './style/sidebar/search-bar/search-bar.scss';
-import './style/footer/footer.scss';
