1f474d3389cabfb32a42f0f0b57cbcd184090d86
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2018-06-16 16:24:27 +0900

    react-router-configでネストしたルーティングを設定

--- src/routing.js
+++ src/routing.js
@@ -1,6 +1,8 @@
 import Top from './container/Top';
 import CallBack from './container/SpotifyCallBack';
 import Home from './container/Home';
+import SearchBox from './components/SearchBox';
+import SearchResult from './components/SearchResult';
 import About from './container/About';
 import NotFound from './components/NotFound';
 
@@ -17,6 +19,19 @@ const routes = [
   {
     path: '/home',
     component: Home,
+    routes: [
+      {
+        path: '/search',
+        component: SearchBox,
+        routes: [
+          {
+            path: '/result',
+            exact: true,
+            component: SearchResult,
+          },
+        ],
+      },
+    ],
   },
   {
     path: '/about',
