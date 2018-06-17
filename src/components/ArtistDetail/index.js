f40536a40cca37e8bdbfe7669cb5f5c74f2ff495
Tomohiro Soga <tomohiro_soga@yahoo.co.jp>
2018-06-17 10:47:09 +0900

    アーティスト画像をネガティブマージン使って横いっぱいに広がるようにした

--- src/components/ArtistDetail/index.js
+++ src/components/ArtistDetail/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  width: 100vw;
+  height: 60vh;
+  margin-left: -15vw;
+  padding-left: 15vw;
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-position: 50%;
+
+  ::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: linear-gradient(transparent -30%,#181818);
+  }
+`;
 
 const ArtistDetail = (props) => {
   const {
     searchResult: {
-      artists,
+      artists: {
+        items,
+      },
     },
     match: {
       params: {
@@ -12,9 +36,14 @@ const ArtistDetail = (props) => {
       },
     },
   } = props;
+  const [artist] = items.filter(item => item.id === artistId);
 
   return (
-    <div />
+    <React.Fragment>
+      <Header
+        image={artist.images[0].url}
+      />
+    </React.Fragment>
   );
 };
 
@@ -24,7 +53,9 @@ ArtistDetail.propTypes = {
   }).isRequired,
   accessToken: PropTypes.string.isRequired,
   refreshToken: PropTypes.string.isRequired,
-  match: PropTypes.object.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.object.isRequired,
+  }).isRequired,
 };
 
-export default ArtistDetail;
+export default withRouter(ArtistDetail);
